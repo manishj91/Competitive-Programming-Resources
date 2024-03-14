325aa37adcbf3603c9dff82041421b462fbfc2fa
Manish Jha <manishkumarjhaece@gmail.com>
2024-03-14 12:29:47 +0530

    0-1 BFS

---
+++ Graphs/BFS01.cpp
@@ -0,0 +1,18 @@
+vector<ll> dist(n,inf);
+dist[src] = 0;
+deque<int> q;
+
+while(!q.empty())
+{
+    int node = q.front();
+    q.pop_front();
+
+    for(auto &[ch,w]: adj[node])
+    {
+        if(dist[ch]<=dist[node]+w) continue;
+        dist[ch] = dist[node]+w;
+
+        if(w==1) q.push_back(ch);
+        else q.push_front(ch);
+    }
+}
